5b89e00ac3f04f38ea4e35104e40f7493b3fe97b
The Anh Nguyen <anhnguyenthe29112004@gmail.com>
2025-02-26 21:56:01 +0700

    add views and public folder

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('home')
+  getHello() {
+    const message1 = this.appService.getHello();
+    return {
+      message: message1
+    }
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UserController],
+  imports: [MongooseModule.forRoot('mongodb+srv://anhnguyenthe29112004:Lan10082004%40@cluster0.sromp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule { }
