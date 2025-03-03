5a225f061016c9c2c59b108f339718f076a5bedf
The Anh Nguyen <anhnguyenthe29112004@gmail.com>
2025-03-04 05:49:53 +0700

    use soft delete plugin and timestamp plugin

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { User } from './schemas/user.schema';
+import { User, UserDocument } from './schemas/user.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { compareSync, genSaltSync, hashSync } from 'bcryptjs';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) { }
+  constructor(@InjectModel(User.name) private userModel: SoftDeleteModel<UserDocument>) { }
 
   getHashPassword = (password: string) => {
     const salt = genSaltSync(10);
@@ -53,6 +54,6 @@ export class UsersService {
   }
 
   async remove(id: string) {
-    return await this.userModel.deleteOne({ _id: id });
+    return await this.userModel.softDelete({ _id: id });
   }
 }
